9e01d83a823651b304c419e43a99201aef047f45
prasanthgunase1 <prasanth.gunase1@tigeranalytics.com>
2025-11-03 16:00:11 +0530

    fixed issue in revenue graph and add total P&l graph

--- src/components/Dashboards/TrendComponent.jsx
+++ src/components/Dashboards/TrendComponent.jsx
@@ -4,37 +4,17 @@ import Grid2 from "@mui/material/Grid2";
 import classes from "./TrendComponent.module.scss";
 import DepositLoan from "./Trends/DepositLoan";
 import RevenueGraph from "./Trends/RevenueGraph";
+import TotalProfitAndLossRelation from "./Trends/TotalProfitAndLossRelation";
 
 export default function TrendComponent() {
     return (
         <Box className={classes.trendComponent}>
-            {/* Row 1 */}
-            <Grid2 container spacing={2}>
-                <Grid2 xs={12} md={6}>
-                    <DepositLoan />
-                </Grid2>
-            </Grid2>
+            <DepositLoan />
+            <RevenueGraph />
+            <TotalProfitAndLossRelation />
 
-            {/* Row 2 */}
-            <Grid2 container spacing={2}>
-                <Grid2 xs={12} md={4}>
-                    {/* <RevenueGraph /> */}
-                    {/* <DepositLoan /> */}
-                </Grid2>
-            </Grid2>
-
-            {/* Row 3 */}
-            <Grid2 container spacing={2}>
-                {/* (No components right now) */}
-            </Grid2>
-
-            {/* Row 4 */}
-            <Grid2 container spacing={2}>
-                {/* (No components right now) */}
-            </Grid2>
-            
         </Box>
 
-      
+
     );
 }
