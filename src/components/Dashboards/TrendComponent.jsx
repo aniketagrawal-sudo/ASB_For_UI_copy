0a16a786a7e59b66c4bebfe210c39069f6cfd73c
prasanthgunase1 <prasanth.gunase1@tigeranalytics.com>
2025-11-04 11:40:49 +0530

    add:Client information

--- src/components/Dashboards/TrendComponent.jsx
+++ src/components/Dashboards/TrendComponent.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import Grid2 from "@mui/material/Grid2";
 import classes from "./TrendComponent.module.scss";
 import DepositLoan from "./Trends/DepositLoan";
 import RevenueGraph from "./Trends/RevenueGraph";
 import TotalProfitAndLossRelation from "./Trends/TotalProfitAndLossRelation";
+import ClinetInformationComponent from "./Trends/ClinetInformationComponent";
 
 export default function TrendComponent() {
     return (
-        <Box className={classes.trendComponent}>
-            <DepositLoan />
-            <RevenueGraph />
-            <TotalProfitAndLossRelation />
+        <>
+            <Box className={classes.trendComponent}>
+                <DepositLoan />
+                <RevenueGraph />
+                <TotalProfitAndLossRelation />
 
-        </Box>
+
+            </Box>
+            {/* <Box className={classes.depositBox}> */}
+            <Typography variant="h6" margin={1}>
+                Client Information
+            </Typography>
+            {/* </Box> */}
+
+            <Box className={classes.trendComponent}>
+                <Typography variant="body2" component="div">
+                    <ClinetInformationComponent />
+                </Typography>
+
+            </Box>
+        </>
+
 
 
     );
